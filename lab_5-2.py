1610792b925e9bcffc1a480df4c08f77c57d3ece
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-09-20 07:03:52 -0700

    Submit Lab 5-2

---
+++ lab_5-2.py
@@ -0,0 +1,24 @@
+# Author: CRS 9/20/21
+# For each of the following assignment statements copy the statement as a comment, then write the other way you could write the statement using augmented assignments or not using augmented assignments.
+# 1. x+=5
+# 2. y=y-3 
+# 3. z**=2
+# 4. w=w%2 
+# 5. t=t/5 
+# 6. a*=12
+# 7. i=i//7
+
+# 1
+x = x + 5
+# 2
+y -= 3
+# 3
+z = z ** 2
+# 4
+w %= 2
+# 5
+t /= 5
+# 6
+a = a * 12
+# 7
+i //= 7
